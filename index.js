9e83359fb753bc63bd4d2e554c9bd8f9bc8d02c2
unknown <zeeshanmehdi18@gmail.com>
2020-06-03 18:03:59 +0500

    report added

--- index.js
+++ index.js
@@ -13,17 +13,42 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
-
+let ejs = require("ejs");
+let pdf = require("html-pdf");
+let path = require("path");
+app.set('view engine', 'ejs');
 //app.use(app.engine('ejs'));
 
 app.get('/', function(req, res) {
- res.render('index.ejs');
+ res.render('index',{user:{email:'abcd'}});
 });  
 
 app.post('/mail',function(req,res){
     console.log(req.body);
 
-    res.send(req.body);
+    let user = Json.parse(req.body);
+    ejs.renderFile(path.join(__dirname, './views/', "report.ejs"), {user:user}, (err, data) => {
+      if (err) {
+            res.send(err);
+      } else {
+          let options = {
+              "height": "11.25in",
+              "width": "8.5in",
+              "header": {
+                  "height": "20mm"
+              },
+              "footer": {
+                  "height": "20mm",
+              },
+          };
+          pdf.create(data, options).toFile("report"+user.email+".pdf", function (err, data) {
+              if (err) {
+                  res.send(err);
+              } else {
+                  res.send("File created successfully");
+              }
+          });
+      }
 });
 
 startServer();
