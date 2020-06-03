0bace35f3f549db111b6499b1b7b182fc52cbbad
unknown <zeeshanmehdi18@gmail.com>
2020-06-03 18:48:24 +0500

    report added

--- index.js
+++ index.js
@@ -27,30 +27,37 @@ app.post('/mail',function(req,res){
     console.log(req.body);
 
     let user = Json.parse(req.body);
-    ejs.renderFile(path.join(__dirname, './views/', "report.ejs"), {user:user}, (err, data) => {
-      if (err) {
-            res.send(err);
-      } else {
-          let options = {
-              "height": "11.25in",
-              "width": "8.5in",
-              "header": {
-                  "height": "20mm"
-              },
-              "footer": {
-                  "height": "20mm",
-              },
-          };
-          pdf.create(data, options).toFile("report"+user.email+".pdf", function (err, data) {
-              if (err) {
-                  res.send(err);
-              } else {
-                  res.send("File created successfully");
-              }
-          });
-      }
-    
-})});
+    sendEmail(user);
+
+    res.send('mail sent successfully');
+   });
+
+function sendEmail(user){
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'smartsolutions343@gmail.com',
+      pass: 'zeeshan1998'
+    }
+  });
+
+  const data = await ejs.renderFile(path.join(__dirname, './views/', "report.ejs"), { user:user });
+  
+  var mailOptions = {
+    from: 'smartsolutions343@gmail.com',
+    to: user.email,
+    subject: 'Weekly Report by BNB Sniper Pro',
+    html: data
+  };
+  
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+}
 
 startServer();
 
